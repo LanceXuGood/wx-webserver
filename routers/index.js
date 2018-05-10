415d8df4dc3ea181af8aa7153fe234fd883257d0
lance <lancexugood@gmail.com>
2018-05-10 18:26:52 +0800

    微信服务器测试-正式token验证

--- routers/index.js
+++ routers/index.js
@@ -32,7 +32,7 @@ router.post('wx/jsSdk', async (ctx, next) =>{
   const nonce_str = baseConfig.nonce_str; // 密钥，字符串任意，可以随机生成
   const timestamp = new Date().getTime();// 时间戳
   const url = ctx.request.body.url; // 使用接口的url链接，不包含#后的内容
-
+  console.log(url)
   // 将请求以上字符串，先按字典排序，再以'&'拼接，如下：其中j > n > t > u，此处直接手动排序
   const str = 'jsapi_ticket=' + js_ticketData.body.ticket + '&noncestr=' + nonce_str + '×tamp=' + timestamp + '&url=' + url;
 
